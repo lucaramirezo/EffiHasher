d2e90f07b4ab1a5536bb2ca292fc0a4e9db53f08
duo <duoxu1987@gmail.com>
2024-01-21 19:59:48 +0100

    Implementado opción de incrementar capacidad de la tabla

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,10 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Hashstable tabla = new Hashstable(10,0.7);
+		HashTable tabla = new HashTable(10,0.7);
 		for ( int i = 0; i < 7; i++) {
 			tabla.put( new KeyValue("mat"+ i, new Moto("mat"+i, "marca"+i)));
 		}
-
 	}
 
 }
